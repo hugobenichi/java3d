452db438ebf7026140b01766556f44580c6f79e6
Hugo Benichi <hugobenichi@gmail.com>
2018-05-10 18:21:24 +0200

    rendering: opengl: use element array indices

--- src/Game.java
+++ src/Game.java
@@ -26,6 +26,10 @@ interface K {
   int offset0 = 0;
 
   int attr0 = 0;
+
+  int vertex_per_triangle = 3;
+
+  int gl_null = 0;
 }
 
 // Static functions for reducing GL syntax bloat.
@@ -40,30 +44,48 @@ class GLUtil {
     vaoBind(0);
   }
 
-  static void vboBind(int id) {
+  static void vboArrayBufferBind(int id) {
     GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, id);
   }
 
-  static void vboUnbind() {
-    vboBind(0);
+  static void vboArrayBufferUnbind() {
+    vboArrayBufferBind(0);
+  }
+
+  static void vboElementArrayBufferBind(int id) {
+    GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, id);
+  }
+
+  static void vboElementArrayBufferUnbind() {
+    vboElementArrayBufferBind(0);
   }
 
   static void vertexAttribArrayBind(int id) {
-		GL20.glEnableVertexAttribArray(id);
+    GL20.glEnableVertexAttribArray(id);
   }
 
   static void vertexAttribArrayUnbind(int id) {
-		GL20.glDisableVertexAttribArray(id);
+    GL20.glDisableVertexAttribArray(id);
   }
 
-	static void attributeStore(int attrId, float[] data) {
-		int vboId = GLObjects.allocVbo();
-    vboBind(vboId);
-		FloatBuffer buffer = BufferUtil.make(data);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
-		GL20.glVertexAttribPointer(attrId, 3, GL11.GL_FLOAT, false, 0, 0); // HARDCODED !
-    vboUnbind();
-	}
+  static void attributeStore(int attrId, float[] data) {
+    int vboId = GLObjects.allocVbo();
+    vboArrayBufferBind(vboId);
+    FloatBuffer buffer = BufferUtil.make(data);
+    GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+    // TODO: separate vbo loading code above from attribute binding code below
+    // TODO: what is this 'false' parameter ??
+    GL20.glVertexAttribPointer(attrId, K.vertex_per_triangle, GL11.GL_FLOAT, false, K.gl_null, K.gl_null);
+    vboArrayBufferUnbind();
+  }
+
+  static void bindIndices(int[] indices) {
+    int vboId = GLObjects.allocVbo();
+    vboElementArrayBufferBind(vboId);
+    IntBuffer buffer = BufferUtil.make(indices);
+    GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+    //vboElementArrayBufferUnbind(); // Why can't I unbind this ??
+  }
 }
 
 // Static function for managing array of ints/floats
@@ -71,30 +93,37 @@ class BufferUtil {
   // TODO: consider pooling buffers
 
   static IntBuffer make(int[] data) {
-		IntBuffer b = BufferUtils.createIntBuffer(data.length);
-		b.put(data);
-		b.flip();
-		return b;
+    IntBuffer b = BufferUtils.createIntBuffer(data.length);
+    b.put(data);
+    b.flip();
+    return b;
   }
 
   static FloatBuffer make(float[] data) {
-		FloatBuffer b = BufferUtils.createFloatBuffer(data.length);
-		b.put(data);
-		b.flip();
-		return b;
+    FloatBuffer b = BufferUtils.createFloatBuffer(data.length);
+    b.put(data);
+    b.flip();
+    return b;
   }
 }
 
 // Game configs
 interface Config {
-	int WIDTH           = 1280;
-	int HEIGHT          = 720;
-	int FPS_CAP         = 60;
-	String TITLE        = "Our First Display";
+  int WIDTH           = 1280;
+  int HEIGHT          = 720;
+  int FPS_CAP         = 60;
+  String TITLE        = "Our First Display";
 }
 
 // Geometry data
 interface Data {
+  float[] vertices2 = {
+      // Left bottom triangle
+      -0.5f,      0.5f,       0f,
+      -0.5f,      -0.5f,      0f,
+      0.5f,       -0.5f,      0f,
+      0.5f,       0.5f,       0f,
+  };
   float[] vertices = {
       // Left bottom triangle
       -0.5f,      0.5f,       0f,
@@ -105,18 +134,23 @@ interface Data {
       0.5f,       0.5f,       0f,
       -0.5f,      0.5f,       0f
   };
+
+  int[] indices = {
+    0, 1, 3,
+    3, 1, 2,
+  };
 }
 
 // Used to track Vertex Array Object data.
 class Model {
-	int vaoId;
-	int vertexCount;
+  int vaoId;
+  int vertexCount;
 }
 
 // The main class
 public class Game {
 
-	public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
 
     // DISPLAY MANAGEMENT
@@ -124,14 +158,14 @@ public class Game {
     Display.create(new PixelFormat(), new ContextAttribs(3, 2).withForwardCompatible(true).withProfileCore(true));
     Display.setTitle(Config.TITLE);
 
-		GL11.glViewport(0, 0, Config.WIDTH, Config.HEIGHT);
+    GL11.glViewport(0, 0, Config.WIDTH, Config.HEIGHT);
 
 
     // Geometry startup
-		Model model = modelMake(Data.vertices);
+    Model model = modelMake(Data.vertices, Data.indices);
 
 
-		while (!Display.isCloseRequested()) {
+    while (!Display.isCloseRequested()) {
       // TODO: get input
       // TODO: run game logic
 
@@ -140,34 +174,36 @@ public class Game {
       GL11.glClearColor(1, 0, 1, 1); // RGBA
 
       // Render models
-			modelRender(model);
+      modelRender(model);
 
       // Display sync
       Display.sync(Config.FPS_CAP);
       Display.update();
-		}
+    }
 
 
     // Cleanup
     GLObjects.freeAll();
-		Display.destroy();
-	}
+    Display.destroy();
+  }
 
-	static Model modelMake(float[] positions) {
-		int vaoId = GLObjects.allocVao();
+  static Model modelMake(float[] positions, int[] indices) {
+    int vaoId = GLObjects.allocVao();
     GLUtil.vaoBind(vaoId);
-		GLUtil.attributeStore(K.attr0, positions);
-		GLUtil.vaoUnbind();
+    GLUtil.bindIndices(indices);
+    GLUtil.attributeStore(K.attr0, positions);
+    GLUtil.vaoUnbind();
     Model m = new Model();
     m.vaoId = vaoId;
-    m.vertexCount = positions.length / 3;
+    m.vertexCount = indices.length;
     return m;
-	}
+  }
 
-	static void modelRender(Model model) {
+  static void modelRender(Model model) {
     GLUtil.vaoBind(model.vaoId);
     GLUtil.vertexAttribArrayBind(K.attr0); // CLEANUP: hardcoded ! put this into the model instead
-		GL11.glDrawArrays(GL11.GL_TRIANGLES, K.offset0, model.vertexCount);
+    //GL11.glDrawArrays(GL11.GL_TRIANGLES, K.offset0, model.vertexCount); // No indices rendering
+    GL11.glDrawElements(GL11.GL_TRIANGLES, model.vertexCount, GL11.GL_UNSIGNED_INT, K.offset0); // Indices rendering
     GLUtil.vertexAttribArrayUnbind(K.attr0);
     GLUtil.vaoUnbind();
   }
@@ -177,23 +213,23 @@ public class Game {
 // Tracks VBO and VAO ids
 class GLObjects {
   // TODO: tracks ids more efficiently without boxing ids !
-	static List<Integer> vaos = new ArrayList<Integer>();
-	static List<Integer> vbos = new ArrayList<Integer>();
+  static List<Integer> vaos = new ArrayList<Integer>();
+  static List<Integer> vbos = new ArrayList<Integer>();
 
   static int allocVao() {
-		int id = GL30.glGenVertexArrays();
-		vaos.add(id);
+    int id = GL30.glGenVertexArrays();
+    vaos.add(id);
     return id;
   }
 
   static int allocVbo() {
-		int id = GL15.glGenBuffers();
-		vbos.add(id);
+    int id = GL15.glGenBuffers();
+    vbos.add(id);
     return id;
   }
 
-	static void freeAll() {
+  static void freeAll() {
     vaos.forEach(GL30::glDeleteVertexArrays);
     vbos.forEach(GL15::glDeleteBuffers);
-	}
+  }
 }
